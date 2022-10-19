1cb974ab623ccee1b09684ab2a5982dd5bf877ed
Anna <anysofronova@yandex.ru>
2022-10-19 16:33:28 +0300

    feat: 8 kyu

---
+++ 8 kyu/Remove String Spaces.ts
@@ -0,0 +1,3 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+const noSpace = (x: string) => x.replace(/ /g, "");
---
+++ 8 kyu/Return Negative.ts
@@ -0,0 +1,3 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+const makeNegative = (num: number) => -Math.abs(num);
