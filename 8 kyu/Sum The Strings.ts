87e472161653b67ced19934628cf0d4b15012751
Anna <anysofronova@yandex.ru>
2022-12-19 10:35:51 +0300

    feat: 8 kyu

---
+++ 8 kyu/Sum The Strings.ts
@@ -0,0 +1,3 @@
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+const sumStr = (a: string, b: string) => String(Number(a) + Number(b));
